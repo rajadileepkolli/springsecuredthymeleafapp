8934dcf42b0022c7290ee68ca97d5dc17b0dbe29
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2016-06-04 12:03:58 +0530

    Using undertow instead of tomcat as embedded container and enabled method level security

--- src/main/java/com/learning/securedapp/configuration/SecurityConfiguration.java
+++ src/main/java/com/learning/securedapp/configuration/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.learning.securedapp.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled= true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
