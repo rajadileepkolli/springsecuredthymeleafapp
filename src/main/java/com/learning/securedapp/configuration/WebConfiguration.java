b446aa44cc0b9e4e4f98fa7b2465ccbc3bc78f43
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2016-06-04 13:08:53 +0530

    Revert back to tomcat embeddedContainer and enabled http to https redirect

---
+++ src/main/java/com/learning/securedapp/configuration/WebConfiguration.java
@@ -0,0 +1,57 @@
+package com.learning.securedapp.configuration;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 
+ * @author rajakolli
+ * 
+ * To redirect from HTTP to HTTPS let us configure TomcatEmbeddedServletContainerFactory
+ * bean in our WebConfiguration.java
+ * 
+ * keystore can be generated using the command keytool -genkey -alias securedapp
+ * -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore securekeystore.p12 -validity 365
+ */
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Value("${server.port:8443}")
+    private int serverPort;
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(serverPort);
+
+        return connector;
+    }
+}
--- src/test/java/com/learning/securedapp/ApplicationTests.java
+++ src/test/java/com/learning/securedapp/ApplicationTests.java
@@ -3,14 +3,26 @@ package com.learning.securedapp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	/*@LocalServerPort
+    private int port;
+
+    @Test
+    public void testHome() throws Exception {
+        TestRestTemplate testRestTemplate = new TestRestTemplate(HttpClientOption.SSL);
+        ResponseEntity<String> entity = testRestTemplate
+                .getForEntity("https://localhost:" + 8443, String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(entity.getBody()).isEqualTo("Hello World");
+    }*/
 }
