1caea172834fddfbb7140a53b20e837036701f0c
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2016-06-06 01:40:10 +0530

    Added CustomException Bean as part of Data Abstration

--- src/main/java/com/learning/securedapp/exception/SecuredAppException.java
+++ src/main/java/com/learning/securedapp/exception/SecuredAppException.java
@@ -3,9 +3,38 @@ package com.learning.securedapp.exception;
 public class SecuredAppException extends Exception {
 
     private static final long serialVersionUID = 8900583085166149180L;
+    
+    private SecuredAppExceptionBean faultBean;
 
     public SecuredAppException(String message) {
         super(message);
+    }
+
+    public SecuredAppException(SecuredAppExceptionBean faultBean) {
+        super(faultBean.getFaultString());
+        this.faultBean = faultBean;
+    }
+    
+    public SecuredAppException(String message, SecuredAppExceptionBean faultinfo) {
+        super(message);
+        this.faultBean = faultinfo;
+    }
+    
+    public SecuredAppException(String message, SecuredAppExceptionBean faultinfo,
+            Throwable cause) {
+        super(message, cause);
+        this.faultBean = faultinfo;
+    }
+    
+    public SecuredAppException(String code, String message) {
+        super(message);
+        this.faultBean = new SecuredAppExceptionBean();
+        this.faultBean.setFaultCode(code);
+        this.faultBean.setFaultString(message);
+    }
+
+    public SecuredAppExceptionBean getFaultBean() {
+        return faultBean;
     }
 
 }
---
+++ src/main/java/com/learning/securedapp/exception/SecuredAppExceptionBean.java
@@ -0,0 +1,9 @@
+package com.learning.securedapp.exception;
+
+import lombok.Data;
+
+@Data
+public class SecuredAppExceptionBean {
+    private String faultCode;
+    private String faultString;
+}
