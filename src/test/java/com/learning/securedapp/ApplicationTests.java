bc892e80aed242cace094bfb2ea8306bd81e7b53
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2016-12-14 05:52:58 +0530

    Added svn:ignore and small testing

--- src/test/java/com/learning/securedapp/ApplicationTests.java
+++ src/test/java/com/learning/securedapp/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.learning.securedapp;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -11,8 +12,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientOption;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,37 +30,46 @@ import org.springframework.web.context.WebApplicationContext;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(value = "test")
-public class ApplicationTests {
+public class ApplicationTests
+{
 
-	@Rule
-	public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("build/generated-snippets");
+    @Rule
+    public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation(
+            "build/generated-snippets");
 
-	@Autowired
-	private WebApplicationContext context;
+    @Autowired
+    private WebApplicationContext context;
 
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	private RestDocumentationResultHandler document;
+    private RestDocumentationResultHandler document;
 
-	@Before
-	public void setUp() {
-		this.document = document("{method-name}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()));
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-				.apply(documentationConfiguration(this.restDocumentation)).alwaysDo(this.document).build();
-	}
+    @Before
+    public void setUp()
+    {
+        this.document = document("{method-name}", preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()));
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .alwaysDo(this.document).build();
+    }
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads()
+    {
+    }
 
-	/*
-	 * @LocalServerPort private int port;
-	 * 
-	 * @Test public void testHome() throws Exception { TestRestTemplate
-	 * testRestTemplate = new TestRestTemplate(HttpClientOption.SSL);
-	 * ResponseEntity<String> entity = testRestTemplate
-	 * .getForEntity("https://localhost:" + 8443+"/login.html", String.class);
-	 * assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK); //
-	 * assertThat(entity.getBody()).isEqualTo("Hello World"); }
-	 */
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testHome() throws Exception
+    {
+        TestRestTemplate testRestTemplate = new TestRestTemplate(HttpClientOption.SSL);
+        ResponseEntity<String> entity = testRestTemplate
+                .getForEntity("https://localhost:" + 8443 + "/login.html", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK); //
+        assertThat(entity.getBody()).isEqualTo("Hello World");
+    }
+
 }
